ab4d5fc341197d59c26f21d2a9b0b22dfd8bca79
Swag-blips <coderblip@gmail.com>
2024-07-16 17:08:32 +0100

    go code

--- investment.go
+++ investment.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import "math"
 
 func main(){
-	fmt.Print(1+1)
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+
+	var futureValue = float64(investmentAmount) * math.Pow((1 + expectedReturnRate / 100), float64(years))
 }
 
