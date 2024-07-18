010165263ae5441aac8f73316e4d91535302b0db
Swag-blips <coderblip@gmail.com>
2024-07-18 13:29:12 +0100

    bank app swich case statements, for loops

--- bank.go
+++ bank.go
@@ -1,9 +1,57 @@
 package main
 
-
 import "fmt"
 
+func main() {
 
-func main(){
-	
+	var accountBalance = 1000.0
+	fmt.Println("Welcome to Go Bank!")
+
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw balance")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+	for {
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is", accountBalance)
+		case 2:
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be Greater than 0")
+				continue
+			}
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New amount:", accountBalance)
+		case 3:
+			fmt.Print("Your withdrawal: ")
+			var withdrawalAmount float64
+			fmt.Scan(&withdrawalAmount)
+			if withdrawalAmount <= 0 {
+				fmt.Println("Invalid amount. Must be Greater than 0")
+				continue
+			}
+
+			if withdrawalAmount > accountBalance {
+				fmt.Println("Invalid amount, You cant withdraw more than you have")
+				continue
+			}
+			accountBalance -= withdrawalAmount
+			fmt.Println("Balance updated! New amount:", accountBalance)
+		default:
+			fmt.Println("Goodbye")
+			fmt.Println("Thanks for choosing Go bank")
+			return
+		}
+
+	}
+
 }
