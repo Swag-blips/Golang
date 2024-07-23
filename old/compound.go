ea40eb11578c92fd071649f8a96767ddd30aab6d
Swag-blips <coderblip@gmail.com>
2024-07-23 12:56:19 +0100

    new changes

---
+++ old/compound.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func madin() {
+	principal, err := getUserInputAlt("What is your Principal: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	interestRate, err1 := getUserInputAlt("What is your Interest rate (in %): ")
+	if err1 != nil {
+		fmt.Println(err1)
+		return
+	}
+
+	numberOfTimes, err2 := getUserInputAlt("What is the number of times interest is compounded per year: ")
+	if err2 != nil {
+		fmt.Println(err2)
+		return
+	}
+
+	numberOfYears, err3 := getUserInputAlt("What is the number of years: ")
+	if err3 != nil {
+		fmt.Println(err3)
+		return
+	}
+
+	compoundInterest := calculateCompoundInterest(principal, interestRate, numberOfTimes, numberOfYears)
+	fmt.Printf("The future value of the investment is: %.2f\n", compoundInterest)
+}
+
+func getUserInputAlt(infoText string) (float64, error) {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+
+	if userInput <= 0 {
+		return 0, errors.New("input must be greater than 0")
+	}
+
+	return userInput, nil
+}
+
+func calculateCompoundInterest(principal, interestRate, numberOfTimes, numberOfYears float64) float64 {
+	ratePerTime := (interestRate / 100) / numberOfTimes
+	exponent := numberOfTimes * numberOfYears
+	result := principal * math.Pow((1+ratePerTime), exponent)
+
+	return result
+}
---
+++ old/investment.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const inflationRate = 2.5
+
+func mainasda() {
+
+	var investmentAmount float64
+	var years float64
+	expectedReturnRate := 5.5
+
+	outputTexta("Investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	outputTexta("How many years?")
+	fmt.Scan(&years)
+
+	outputTexta("What is your expected return rate? ")
+	fmt.Scan(&expectedReturnRate)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	// formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	// formattedRFV := fmt.Sprintf("The value (inflaftion adjusted is): %.0f\n", futureRealValue)
+	// fmt.Println("The future value is :", futureValue)
+	fmt.Printf(`Future Value: %.1f
+	The value (inflaftion adjusted is) : %.0f`, futureValue, futureRealValue)
+
+	// fmt.Print(formattedFV, formattedRFV)
+}
+
+func outputTexta(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
+	// return fv, rfv
+
+	return fv, rfv
+}
