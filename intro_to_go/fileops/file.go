5cf0563fd5b6102fe7138b09fd5a957d4dac9bc0
Swag-blips <coderblip@gmail.com>
2024-07-27 23:57:06 +0100

    changes

---
+++ intro_to_go/fileops/file.go
@@ -0,0 +1,31 @@
+package fileops
+
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func getFloatFromFile(fileName string) (float64, error) {
+	data, err := os.ReadFile(fileName)
+
+	if err != nil {
+		return 1000, errors.New("failed to read file")
+	}
+	valueText := string(data)
+	value, err := strconv.ParseFloat(valueText, 64)
+
+	if err != nil {
+		return 0, errors.New("failed to parse stored value")
+	}
+
+	return value, nil
+
+}
+
+func writeFloatToFile(value float64, fileName string) {
+	valuetext := fmt.Sprint(value)
+	os.WriteFile(fileName, []byte(valuetext), 0644)
+}
