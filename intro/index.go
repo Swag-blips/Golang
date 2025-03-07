4b8e101cf378a58d9ab31403e787b073b7aa6890
Swag-blips <coderblip@gmail.com>
2025-03-07 08:19:45 +0100

    feat: logical operators

--- intro/index.go
+++ intro/index.go
@@ -1,30 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 const user = "admin"
 
 func main() {
-
-	var num1 = 2000
-
-	fmt.Println(num1 + 2)
-	// delcaring variables with var keyword + type being inferred
-	var name = "John Doe"
-
-	// declaring variable while inferring types
-	age := 1
-
-	// manually assinging type
-	var nameWithAssignedType float32 = 12.5
-
-	// multiple variable declaration
-
-	fmt.Println(name, age, nameWithAssignedType)
-
-	fmt.Println(strings.Compare(name, user))
 
 }
