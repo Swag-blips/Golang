f6db02b738b4580e49b7fdd3b532e69c5408ee33
Swag-blips <coderblip@gmail.com>
2025-03-08 10:49:05 +0100

    feat: reverse strings golang

--- intro/index.go
+++ intro/index.go
@@ -1,15 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+type Person struct {
+	name   string
+	age    int
+	job    string
+	salary int
+}
 
 func main() {
-	num := []int{10, 20, 30, 40, 50}
+	var userOne Person
+	userOne.name = "Wayne"
+	userOne.age = 20
+	userOne.job = "Software engineer"
+	userOne.salary = 100000
 
-	num = append(num, 30, 40, 50, 60)
+	// greet("swag")
 
-	fmt.Println(num)
+	reverseString("swag")
 
-	slice := make([]int, 5, 20)
+}
 
-	fmt.Println(slice)
+func greet(name string) {
+	fmt.Println("Hello", name)
+}
+
+func reverseString(value string) string {
+	splittedString := strings.Split(value, "")
+	fmt.Println(len(splittedString))
+
+	var poppedValue string
+
+	for i := 0; i <= len(value); i++ {
+
+		if len(splittedString) <= 0 {
+			break
+		}
+		fmt.Println(i)
+		fmt.Println("before popped", splittedString)
+		poppedValue = poppedValue + splittedString[len(splittedString)-1]
+
+		splittedString = splittedString[:len(splittedString)-1]
+		fmt.Println("after popped", splittedString, poppedValue)
+
+	}
+
+	return poppedValue
+
 }
