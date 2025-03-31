8d328ddbb54653caf7d4316a2aec32603b6bab35
Swag-blips <coderblip@gmail.com>
2025-03-31 19:28:40 +0100

    feat: added post request for todos

--- apis/main.go
+++ apis/main.go
@@ -7,9 +7,40 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Hello world")
 	app := fiber.New()
 
-	log.Fatal(app.Listen(":4000"))
+	todos := []Todo{}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"message": "Hello world"})
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+
+
+	app.Put("")
+	log.Fatal(app.Listen(":7000"))
 }
